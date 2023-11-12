072a0964af27f82eafbeeff2fd5de0555d218826
MotyaAbormotya <mmtt888@mail.ru>
2023-11-12 14:51:36 +0300

    state machine done

---
+++ TicketTgBot/StateMachine/ChatsRouter.cs
@@ -0,0 +1,27 @@
+namespace TicketTgBot.Bot;
+
+public class ChatsRouter
+{
+    private ChatTransmittedDataPairs _chatTransmittedDataPairs;
+    private ServicesManager _servicesManager;
+
+    public ChatsRouter()
+    {
+        _servicesManager = new ServicesManager();
+        _chatTransmittedDataPairs = new ChatTransmittedDataPairs();
+    }
+
+    public BotMessage Route(long chatId, string textData)
+    {
+        if (_chatTransmittedDataPairs.ContainsKey(chatId) == false)
+        {
+            _chatTransmittedDataPairs.CreateNew(chatId);
+        } 
+
+        TransmittedData transmittedData = _chatTransmittedDataPairs.GetByChatId(chatId);
+
+        Console.WriteLine($"РОУТЕР chatId = {chatId}; State = {transmittedData.State}");
+
+        return _servicesManager.ProcessBotUpdate(textData, transmittedData);
+    }
+}
